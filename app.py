f11e893ed77a2b35a75860376195bddb457a78b7
JYOTI PRASAD <jyotiprasad20062006@gmail.com>
2026-05-26 06:48:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def upload_paper():
     if file and file.filename.endswith('.pdf'):
         try:
             # Streams file straight onto Cloudinary permanent cloud storage
-            upload_result = cloudinary.uploader.upload(file, resource_type="raw", folder="itep_papers")
+            upload_result = cloudinary.uploader.upload(file, resource_type="auto", folder="itep_papers")
             file_url = upload_result.get("secure_url")
 
             academic_year = request.form.get('academicYear')
@@ -272,7 +272,7 @@ def contribute_paper():
 
     if file and file.filename.endswith('.pdf'):
         try:
-            upload_result = cloudinary.uploader.upload(file, resource_type="raw", folder="itep_pending")
+            upload_result = cloudinary.uploader.upload(file, resource_type="auto", folder="itep_pending")
             file_url = upload_result.get("secure_url")
 
             academic_year = request.form.get('academicYear')
