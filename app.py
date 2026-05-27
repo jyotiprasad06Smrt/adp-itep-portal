fe6d1bb9bc981fb797a2f3f117b9535b8530c570
JYOTI PRASAD <jyotiprasad20062006@gmail.com>
2026-05-27 06:38:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,6 +145,7 @@ def send_real_otp_email(receiver_email, otp_code):
 
     try:
         server = smtplib.SMTP_SSL(smtp_server, smtp_port, timeout=10)
+        server.starttls()
         server.login(sender_email, sender_password)
         server.sendmail(sender_email, receiver_email, msg.as_string())
         server.quit()
